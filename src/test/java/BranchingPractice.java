359791d6ff2e79fa93565dc8b65f773787cf7e75
Zebib Teweldebrhan <zizu_ss@yahoo.com>
2021-05-25 19:29:16 -0400

    first commit in review1 branch

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -2,11 +2,14 @@ public class BranchingPractice {
 
     public static void main(String[] args) {
 
-        //created new project
+        // created new project
         // enabled version control
         // added .gitignore with .idea, target folder and .iml file
         // made initial commit by keeping track of all files
         // shared project on github
+
+        // reviewing on new branch
+
 
     }
 
