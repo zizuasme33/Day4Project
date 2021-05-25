253962a41682dc68e81882a12a1634368081f7e5
Zebib Teweldebrhan <zizu_ss@yahoo.com>
2021-05-25 19:23:39 -0400

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -2,6 +2,12 @@ public class BranchingPractice {
 
     public static void main(String[] args) {
 
+        //created new project
+        // enabled version control
+        // added .gitignore with .idea, target folder and .iml file
+        // made initial commit by keeping track of all files
+        // shared project on github
+
     }
 
 }
